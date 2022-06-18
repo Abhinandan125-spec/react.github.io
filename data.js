5183f1f0663ab7eb6bd4f9ac3db22d3bbec4926e
Abhinandan Patra <79634072+Abhinandan125-spec@users.noreply.github.com>
2022-06-18 13:37:23 +0530

    data

---
+++ data.js
@@ -0,0 +1,61 @@
+export const data = [
+    {
+      id: 1,
+      name: "design",
+      title: "Best Offer In Town",
+      desc: "Simply drag and drop photos and videos into your workspace to automatically add them to your Collab Cloud library.",
+      
+      photo: "image-one.png"
+    },
+    {
+        id: 2,
+        name: "development",
+        title: "Fast Delivery",
+        desc: "Simply drag and drop photos and videos into your workspace to automatically add them to your Collab Cloud library.",
+        photo: "image-two.png"
+    },
+    {
+     id: 3,
+     name: "production",
+     title: "Quality Food",
+     desc: "Simply drag and drop photos and videos into your workspace to automatically add them to your Collab Cloud library.",
+     photo: "image-three.png"
+    },
+    {
+        id: 4,
+        name: "photography",
+        title: "All Time Available",
+        desc: "Simply drag and drop photos and videos into your workspace to automatically add them to your Collab Cloud library.",
+        photo: "image-about.png"
+
+    },
+]
+export const users = [
+    {
+      id: 1,
+      logo: "yahoo.png",
+      avatar: "user1.jpeg",
+      comment:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Utconsectetur veniam fugiat labore quaerat possimus laudantium.",
+      name: "Harold Brown",
+      title: "Executive Manager",
+    },
+    {
+      id: 2,
+      logo: "google.png",
+      avatar: "user2.jpeg",
+      comment:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Utconsectetur veniam fugiat labore quaerat possimus laudantium.",
+      name: "Anna Taylor",
+      title: "HR Manager",
+    },
+    {
+      id: 3,
+      logo: "facebook.png",
+      avatar: "user3.jpeg",
+      comment:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Utconsectetur veniam fugiat labore quaerat possimus laudantium.",
+      name: "James Garry",
+      title: "UI Designer",
+    },
+  ];
